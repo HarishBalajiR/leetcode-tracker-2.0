0bc9c95eed5e228c08206b8a338610e8fa8c680b
Harish Balaji <hariahbalaji672@gmail.com>
2026-03-30 14:12:58 +0530

    Create 782-JewelsAndStones

---
+++ 782-JewelsAndStones/782-JewelsAndStones.java
@@ -0,0 +1,12 @@
+// Last updated: 3/30/2026, 2:12:46 PM
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(char ch : jewels.toCharArray()) map.put(ch,map.getOrDefault(ch,0)+1);
+        for(char ch : stones.toCharArray()){
+            if(map.containsKey(ch)) count++;
+        }
+        return count;
+    }
+}
