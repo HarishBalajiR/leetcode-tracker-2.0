bb2e48ae2982e4532eb4c031a229d0a0b9981b4d
Harish Balaji <hariahbalaji672@gmail.com>
2026-03-30 14:11:47 +0530

    Create 2556-ConvertTheTemperature

---
+++ 2556-ConvertTheTemperature/2556-ConvertTheTemperature.cpp
@@ -0,0 +1,12 @@
+// Last updated: 3/30/2026, 2:11:35 PM
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        vector<double> result;
+        double kelvin = celsius + 273.15;
+        double fahrenheit = celsius * 1.80 + 32.00;
+        result.push_back(kelvin);
+        result.push_back(fahrenheit);
+        return result;
+    }
+};
