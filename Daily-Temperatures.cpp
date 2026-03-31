ea874668fdcadda28f444d83c8a8da4e974166c8
Harish Balaji <hariahbalaji672@gmail.com>
2026-03-31 10:30:08 +0530

    [739] [Time Beats: 38.37%] [Memory Beats: 38.65%] - LeetPush

---
+++ Daily-Temperatures.cpp
@@ -0,0 +1,16 @@
+1class Solution {
+2public:
+3    vector<int> dailyTemperatures(vector<int>& temperatures) {
+4        int n = temperatures.size();
+5        vector<int> ans(n,0);
+6        stack<int> st;
+7        for(int index = 0 ; index < n ; index++){
+8            while(!st.empty() && temperatures[st.top()]<temperatures[index]){
+9                ans[st.top()] = index - st.top();
+10                st.pop();
+11            }
+12            st.push(index);
+13        }
+14        return ans;
+15    }
+16};
