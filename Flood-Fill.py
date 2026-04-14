6b51af7c72685867ef5b429d0250b71d31b932f1
Harish Balaji <hariahbalaji672@gmail.com>
2026-04-14 12:12:42 +0530

    [733] [Time Beats: 100.00%] [Memory Beats: 67.04%] - LeetPush

--- Flood-Fill.py
+++ Flood-Fill.py
@@ -1,21 +1,18 @@
 1class Solution {
-2private:
-3    vector<int> diff = {0,1,0,-1,0};
-4    void dfs(vector<vector<int>>& image, int sr, int sc, int R, int C, int orgcolor, int newcolor){
-5        if(image[sr][sc] == newcolor) return;
-6        image[sr][sc] = newcolor;
-7        for(int di = 0 ; di < 4 ; di++){
-8            int adjR = sr + diff[di];
-9            int adjC = sc + diff[di+1];
-10            if(adjR>=0 && adjR<R && adjC>=0 && adjC<C && image[adjR][adjC] == orgcolor){
-11                dfs(image,adjR,adjC,R,C,orgcolor,newcolor);
-12            }
-13        }
-14    }
-15public:
-16    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-17        int R = image.size(), C = image[0].size();
-18        dfs(image,sr,sc,R,C,image[sr][sc],color);
-19        return image;
-20    }
-21};
+2    static int[] diff = {0,1,0,-1,0};
+3    private void dfs(int[][] image, int sr, int sc, int R, int C, int og, int newcolor){
+4        if(image[sr][sc] == newcolor) return;
+5        image[sr][sc] = newcolor;
+6        for(int di = 0 ; di < 4 ; di++){
+7            int adjR = sr + diff[di], adjC = sc + diff[di+1];
+8            if(adjR>=0 && adjR<R && adjC>=0 && adjC<C && image[adjR][adjC] == og){
+9                dfs(image,adjR,adjC,R,C,og,newcolor);
+10            }
+11        }
+12    }
+13    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+14        int R = image.length, C = image[0].length;
+15        dfs(image,sr,sc,R,C,image[sr][sc],color);
+16        return image;
+17    }
+18}
