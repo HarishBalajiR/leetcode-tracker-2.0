577f0529ec28c099e1f3e1347ade701aa1ad1d49
Harish Balaji <hariahbalaji672@gmail.com>
2026-04-15 18:40:21 +0530

    [200] [Time Beats: 78.60%] [Memory Beats: 27.34%] - LeetPush

--- Number-of-Islands.cpp
+++ Number-of-Islands.cpp
@@ -1,28 +1,36 @@
 1class Solution {
 2private:
-3    int diff[5] = {0,1,0,-1,0};
-4    void dfs(vector<vector<char>>& grid, int row, int col, int R, int C){
+3    static constexpr int diff[] = {0,1,0,-1,0};
+4    void bfs(vector<vector<char>>& grid, int row, int col, int R, int C){
 5        grid[row][col] = '0';
-6        for(int di = 0 ; di < 4 ; di++){
-7            int adjR = row + diff[di];
-8            int adjC = col + diff[di+1];
-9            if(adjR>=0 && adjR<R && adjC>=0 && adjC<C && grid[adjR][adjC] == '1'){
-10                dfs(grid,adjR,adjC,R,C);
-11            }
-12        }
-13    }
-14public:
-15    int numIslands(vector<vector<char>>& grid) {
-16        int count = 0;
-17        int R = grid.size(), C = grid[0].size();
-18        for(int row = 0 ; row < R ; row++){
-19            for(int col = 0 ; col < C ; col++){
-20                if(grid[row][col] == '1'){
-21                    dfs(grid,row,col,R,C);
-22                    count++;
-23                }
-24            }
-25        }
-26        return count;
-27    }
-28};
+6        queue<pair<int,int>> q;
+7        q.push(make_pair(row,col));
+8        
+9        while(!q.empty()){
+10            auto val = q.front();
+11            q.pop();
+12            int currentrow = val.first, currentcol = val.second;
+13            for(int di = 0 ; di < 4 ; di++){
+14                int adjR = currentrow + diff[di], adjC = currentcol + diff[di+1];
+15                if(adjR>=0 && adjR<R && adjC>=0 && adjC<C && grid[adjR][adjC] == '1'){
+16                    grid[adjR][adjC] = '0';
+17                    q.push(make_pair(adjR,adjC));
+18                }
+19            }
+20        }
+21    }
+22public:
+23    int numIslands(vector<vector<char>>& grid) {
+24        int R = grid.size(), C = grid[0].size();
+25        int count = 0;
+26        for(int row = 0 ; row < R ;row++){
+27            for(int col = 0 ; col < C ; col++){
+28                if(grid[row][col] == '1'){
+29                    count++;
+30                    bfs(grid,row,col,R,C);
+31                }
+32            }
+33        }
+34        return count;
+35    }
+36};
