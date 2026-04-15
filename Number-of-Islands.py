625b278dbb6bfdf20c5358cca25839d3798fad61
Harish Balaji <hariahbalaji672@gmail.com>
2026-04-15 18:24:36 +0530

    [200] [Time Beats: 5.14%] [Memory Beats: 33.71%] - LeetPush

--- Number-of-Islands.py
+++ Number-of-Islands.py
@@ -1,17 +1,25 @@
-1class Solution:
-2    def dfs(self, grid: List[List[str]], row: int, col: int, R: int, C: int):
-3        diff = [0,1,0,-1,0]
-4        grid[row][col] = '0'
-5        for di in range(4):
-6            adjR,adjC = row+diff[di],col+diff[di+1]
-7            if(adjR>=0 and adjR<R and adjC>=0 and adjC<C and grid[adjR][adjC] == '1'):
-8                self.dfs(grid,adjR,adjC,R,C)
-9
-10    def numIslands(self, grid: List[List[str]]) -> int:
-11        R,C,count = len(grid),len(grid[0]), 0
-12        for row in range(R):
-13            for col in range(C):
-14                if(grid[row][col] == '1'):
-15                    self.dfs(grid,row,col,R,C)
-16                    count+=1
-17        return count
+1import queue
+2class Solution:
+3    def bfs(self, grid: List[List[str]],row: int,col: int,R: int,C: int) -> None:
+4        diff = [0,1,0,-1,0]
+5        grid[row][col] = 0
+6        q = queue.Queue()
+7        q.put([row,col])
+8        while(not q.empty()):
+9            val = q.get()
+10            currentrow,currentcol = val[0],val[1]
+11            for di in range(4):
+12                adjR,adjC = currentrow + diff[di], currentcol + diff[di+1]
+13                if(adjR>=0 and adjR<R and adjC>=0 and adjC<C and grid[adjR][adjC] == "1"):
+14                    grid[adjR][adjC] = "0"
+15                    q.put([adjR,adjC])
+16
+17    def numIslands(self, grid: List[List[str]]) -> int:
+18        R,C = len(grid), len(grid[0])
+19        area = 0
+20        for row in range(R):
+21            for col in range(C):
+22                if(grid[row][col] == "1"):
+23                    self.bfs(grid,row,col,R,C)
+24                    area+=1
+25        return area
