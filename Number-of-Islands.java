99537cc2aada29466049be4e0044749ee93056a2
Harish Balaji <hariahbalaji672@gmail.com>
2026-04-15 18:31:36 +0530

    [200] [Time Beats: 43.24%] [Memory Beats: 90.78%] - LeetPush

--- Number-of-Islands.java
+++ Number-of-Islands.java
@@ -1,25 +1,33 @@
 1class Solution {
-2    int[] diff = {0,1,0,-1,0};
-3    private void dfs(char[][] grid, int row, int col, int R, int C){
+2    static int[] diff = {0,1,0,-1,0};
+3    public void bfs(char[][] grid, int row, int col, int R, int C){
 4        grid[row][col] = '0';
-5        for(int di = 0 ; di < 4 ; di++){
-6            int adjR = row + diff[di], adjC = col + diff[di+1];
-7            if(adjR>=0 && adjR<R && adjC>=0 && adjC<C && grid[adjR][adjC] == '1'){
-8                dfs(grid,adjR,adjC,R,C);
-9            }
-10        }
-11    }
-12    public int numIslands(char[][] grid) {
-13        int count = 0;
-14        int R = grid.length, C = grid[0].length;
-15        for(int row = 0 ; row < R ; row++){
-16            for(int col = 0 ; col < C ; col++){
-17                if(grid[row][col] == '1'){
-18                    dfs(grid,row,col,R,C);
-19                    count++;
-20                }
-21            }
-22        }
-23        return count;
-24    }
-25}
+5        Queue<int[]> q = new LinkedList<>();
+6        q.add(new int[]{row,col});
+7
+8        while(!q.isEmpty()){
+9            int[] val = q.poll();
+10            int currentrow = val[0], currentcol = val[1];
+11            for(int di = 0 ; di < 4 ; di++){
+12                int adjR = currentrow + diff[di], adjC = currentcol + diff[di+1];
+13                if(adjR>=0 && adjR<R && adjC>=0 && adjC<C && grid[adjR][adjC] == '1'){
+14                    grid[adjR][adjC] = '0';
+15                    q.add(new int[]{adjR,adjC});
+16                }
+17            }
+18        }
+19    }
+20    public int numIslands(char[][] grid) {
+21        int R = grid.length, C = grid[0].length;
+22        int area = 0;
+23        for(int row = 0 ; row < R ; row++){
+24            for(int col = 0 ; col < C ; col++){
+25                if(grid[row][col] == '1'){
+26                    area++;
+27                    bfs(grid,row,col,R,C);
+28                }
+29            }
+30        }
+31        return area;
+32    }
+33}
