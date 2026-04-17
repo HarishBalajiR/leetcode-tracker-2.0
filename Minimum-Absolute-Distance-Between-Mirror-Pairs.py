7f5e39782cbd344b8626ca646ea1266a107d73ae
Harish Balaji <hariahbalaji672@gmail.com>
2026-04-17 17:50:57 +0530

    [3761] [Time Beats: 68.22%] [Memory Beats: 91.86%] - LeetPush

--- Minimum-Absolute-Distance-Between-Mirror-Pairs.py
+++ Minimum-Absolute-Distance-Between-Mirror-Pairs.py
@@ -1,21 +1,10 @@
-1class Solution {
-2    private int rev(int num){
-3        int result = 0;
-4        while(num!=0){
-5            result = result*10 + num%10;
-6            num/=10;
-7        }
-8        return result;
-9    }
-10    public int minMirrorPairDistance(int[] nums) {
-11        int mindist = Integer.MAX_VALUE;
-12        HashMap<Integer,Integer> map = new HashMap<>();
-13        for(int index = 0 ; index < nums.length ; index++){
-14            if(map.containsKey(nums[index])){
-15                mindist = Math.min(mindist,Math.abs(map.get(nums[index]) - index));
-16            }
-17            map.put(rev(nums[index]),index);
-18        }
-19        return (mindist!=Integer.MAX_VALUE ? mindist : -1);
-20    }
-21}
+1class Solution:
+2    def minMirrorPairDistance(self, nums: List[int]) -> int:
+3        mindist = 100001
+4        seen = {}
+5        for index in range(len(nums)):
+6            if(nums[index] in seen):
+7                mindist = min(mindist,abs(seen.get(nums[index]) - index))
+8            revnum = int(str(nums[index])[::-1])
+9            seen[revnum] = index
+10        return mindist if mindist!=100001 else -1
