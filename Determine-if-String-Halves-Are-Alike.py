63ed42bc1c1ee3b40203b1b1a9a2df6938391856
Harish Balaji <hariahbalaji672@gmail.com>
2026-04-19 15:03:24 +0530

    [1704] [Time Beats: 6.76%] [Memory Beats: 59.12%] - LeetPush

--- Determine-if-String-Halves-Are-Alike.py
+++ Determine-if-String-Halves-Are-Alike.py
@@ -1,19 +1,11 @@
-1class Solution {
-2    private boolean isvowel(char ch){
-3        ch = Character.toLowerCase(ch);
-4        return ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u';
-5    }
-6    public boolean halvesAreAlike(String s) {
-7        int vcount = 0, N = s.length();
-8        for(int index = 0 ; index < N ; index++){
-9            if(index < N/2){
-10                if(isvowel(s.charAt(index))) vcount++;
-11            }
-12            else{
-13                if(isvowel(s.charAt(index))) vcount--;
-14            }
-15            
-16        }
-17        return vcount==0;
-18    }
-19}
+1class Solution:
+2    def halvesAreAlike(self, s: str) -> bool:
+3        vcount = 0
+4        vowels = ['a','e','i','o','u','A','E','I','O','U']
+5        for index in range(len(s)//2):
+6            if s[index] in vowels:
+7                vcount+=1
+8        for index in range(len(s)//2,len(s)):
+9            if s[index] in vowels:
+10                vcount-=1
+11        return vcount==0
