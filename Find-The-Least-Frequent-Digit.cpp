d2529a78ddb9db75de3c288a0f0451e600bd84a5
Harish Balaji <hariahbalaji672@gmail.com>
2026-04-24 11:50:11 +0530

    [3663] [Time Beats: 100.00%] [Memory Beats: 92.63%] - LeetPush

--- Find-The-Least-Frequent-Digit.cpp
+++ Find-The-Least-Frequent-Digit.cpp
@@ -1,22 +1,18 @@
 1class Solution {
 2public:
 3    int getLeastFrequentDigit(int n) {
-4        unordered_map<int,int> umap;
-5        while(n!=0){
-6            int digit = n%10;
-7            umap[digit]++;
-8            n/=10;
-9        }
-10        int mnfreq = INT_MAX;
-11        for(pair<int,int> val : umap){
-12            mnfreq = min(mnfreq,val.second);
-13        }
-14        int mndigit = 10;
-15        for(auto val : umap){
-16            if(val.second == mnfreq){
-17                mndigit = min(mndigit,val.first);
-18            }
-19        }
-20        return mndigit;
-21    }
-22};
+4        int freq[10] = {0};
+5        while(n>0){
+6            freq[n%10]++;
+7            n/=10;
+8        }
+9        int mindigit = INT_MAX,minfreq = INT_MAX;
+10        for(int digit = 0 ; digit < 10 ; digit++){
+11            if(freq[digit] < minfreq && freq[digit]!=0){
+12                minfreq = freq[digit];
+13                mindigit = digit;
+14            }
+15        }
+16        return mindigit;
+17    }
+18};
